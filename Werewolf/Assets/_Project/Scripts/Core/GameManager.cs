a5729707a0c6c6e6e25fd7c988f49ade67dc582c
Jota <jotapaulo.box@gmail.com>
2026-01-06 13:53:00 -0300

    feat: add GameManager
    add GameManager with global game states

---
+++ Werewolf/Assets/_Project/Scripts/Core/GameManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+namespace Werewolf.Core
+{
+    public enum GameState
+    {
+        Boot,
+        MainMenu,
+        Gameplay,
+        Paused
+    }
+
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+
+        public GameState CurrentState { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start()
+        {
+            SetState(GameState.Boot);
+            Debug.Log($"Game State initialized as: {CurrentState}");
+        }
+
+        public void SetState(GameState newState)
+        {
+            if (CurrentState == newState)
+                return;
+
+            CurrentState = newState;
+
+            Debug.Log($"Game State changed to: {CurrentState}");
+        }
+    }
+}
